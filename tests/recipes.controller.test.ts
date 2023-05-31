881c47e801c9d1d75fbc7c8137f71b38eabb6336
Radosław Kowalski <ladekesl@gmail.com>
2023-05-31 20:49:20 +0200

    Added more unit tests for methods in RecipeController

--- tests/recipes.controller.test.ts
+++ tests/recipes.controller.test.ts
@@ -1,9 +1,17 @@
 import {Request, Response} from 'express';
-import {addRecipeToUser, createRecipe, getAllRecipes, getSavedRecipeIds} from '../controllers/recipes.controller';
 import {RecipeModel} from "../models/Recipes";
 import {IRecipe} from "../types/recipe";
 import {IUser} from "../types/user";
 import {UserModel} from "../models/Users";
+import {ValidationError} from "../utils/errors";
+import {
+    addRecipeToUser,
+    createRecipe, deleteSavedRecipe,
+    getAllRecipes, getRecipeById,
+    getSavedRecipeIds,
+    getSavedRecipes, getUserById
+} from '../controllers/recipes.controller';
+
 
 jest.mock('../models/Recipes');
 jest.mock('../models/Users');
@@ -12,7 +20,9 @@ describe('Recipes Controller', () => {
     let req: Partial<Request>;
     let res: Partial<Response>;
     let mockRecipe: Partial<IRecipe>;
+    let mockRecipes: Partial<IRecipe>[];
     let mockUser: Partial<IUser>
+    let next: jest.Mock;
 
     beforeEach(() => {
         mockRecipe = new RecipeModel();
@@ -141,14 +151,6 @@ describe('Recipes Controller', () => {
             expect(res.json).toHaveBeenCalledWith(mockError);
         });
 
-        it('returns an error when no recipe is found with provided ID', async () => {
-            (RecipeModel.findById as jest.Mock).mockResolvedValue(null);
-
-            await addRecipeToUser(req as Request, res as Response);
-
-            expect(res.status).toHaveBeenCalledWith(404);
-            expect(res.json).toHaveBeenCalledWith({error: 'No recipe found with the provided ID.'});
-        });
 
         it('returns a server error when the save operation fails', async () => {
             const serverError = new Error('ServerError');
@@ -161,6 +163,16 @@ describe('Recipes Controller', () => {
 
             expect(res.status).toHaveBeenCalledWith(500);
             expect(res.json).toHaveBeenCalledWith(serverError);
+        });
+
+        it('returns an error when no recipe is found with provided ID', async () => {
+
+            (RecipeModel.findById as jest.Mock).mockResolvedValue(null);
+
+            await addRecipeToUser(req as Request, res as Response);
+
+            expect(res.status).toHaveBeenCalledWith(404);
+            expect(res.json).toHaveBeenCalledWith({error: 'No recipe found with the provided ID.'});
         });
     });
 
@@ -198,4 +210,172 @@ describe('Recipes Controller', () => {
             expect(res.json).toHaveBeenCalledWith({error: 'User not found'});
         });
     });
+
+    describe('getSavedRecipes', () => {
+        beforeEach(() => {
+            mockUser = {
+                _id: '123',
+                savedRecipes: ['456', '789']
+            };
+
+            mockRecipes = [
+                {_id: '456', name: 'Recipe 1'},
+                {_id: '789', name: 'Recipe 2'}
+            ];
+
+            req = {
+                params: {userID: '123'},
+            };
+
+            res = {
+                status: jest.fn().mockReturnThis(),
+                json: jest.fn()
+            };
+        });
+
+        it('returns saved recipes when the user is found', async () => {
+            (UserModel.findById as jest.Mock).mockResolvedValue(mockUser);
+            (RecipeModel.find as jest.Mock).mockResolvedValue(mockRecipes);
+
+            await getSavedRecipes(req as Request, res as Response);
+
+            expect(res.json).toHaveBeenCalledWith({savedRecipes: mockRecipes});
+
+
+        });
+
+        it('returns an error when the user is not found', async () => {
+            (UserModel.findById as jest.Mock).mockResolvedValue(null);
+
+            await getSavedRecipes(req as Request, res as Response);
+
+            expect(res.status).toHaveBeenCalledWith(404);
+            expect(res.json).toHaveBeenCalledWith({error: 'User not found'});
+
+        });
+    });
+
+    describe('getRecipeById', () => {
+        beforeEach(() => {
+            mockRecipe = {_id: '456', name: 'Recipe 1'};
+
+            req = {
+                params: {recipeID: '456'},
+            };
+
+            res = {
+                status: jest.fn().mockReturnThis(),
+                json: jest.fn()
+            };
+
+            next = jest.fn();
+        });
+
+        it('should return the recipe matching the provided id', async () => {
+
+            (RecipeModel.findById as jest.Mock).mockResolvedValue(mockRecipe);
+
+            await getRecipeById(req as Request, res as Response, next);
+
+            expect(res.json).toHaveBeenCalledWith(mockRecipe);
+        });
+
+        it('should throw a ValidationError when no recipe matches the provided id', async () => {
+            (RecipeModel.findById as jest.Mock).mockResolvedValue(null);
+
+            await getRecipeById(req as Request, res as Response, next);
+
+            expect(next).toHaveBeenCalledWith(new ValidationError('Recipe not found.'));
+        });
+    });
+
+    describe('getUserById', () => {
+        beforeEach(() => {
+            mockUser = {
+                _id: '123',
+                savedRecipes: ['456', '789'],
+                username: 'testowy'
+            };
+
+            req = {
+                params: {userID: '123'},
+            };
+
+            res = {
+                status: jest.fn().mockReturnThis(),
+                json: jest.fn()
+            };
+
+            next = jest.fn();
+        });
+
+        it('should return the user matching the provided id', async () => {
+            (UserModel.findById as jest.Mock).mockResolvedValue(mockUser);
+
+            await getUserById(req as Request, res as Response, next);
+
+            expect(res.json).toHaveBeenCalledWith({username: mockUser.username});
+        });
+
+        it('should throw a ValidationError when no user matches the provided id', async () => {
+            (UserModel.findById as jest.Mock).mockResolvedValue(null);
+
+            await getUserById(req as Request, res as Response, next);
+
+            expect(next).toHaveBeenCalledWith(new ValidationError('User not found.'));
+        });
+    });
+
+    describe('deleteSavedRecipe', () => {
+        beforeEach(() => {
+            mockUser = {
+                _id: '123',
+                savedRecipes: ['456', '789'],
+                username: 'testowy',
+                save: jest.fn().mockResolvedValue(this)
+            };
+
+            mockRecipe = {_id: '456', name: 'Recipe 1'};
+
+            req = {
+                params: {userID: '123', recipeID: '456'},
+            };
+
+            res = {
+                status: jest.fn().mockReturnThis(),
+                json: jest.fn()
+            };
+
+            next = jest.fn();
+        });
+
+        it('should delete the recipe from the user\'s saved recipes', async () => {
+            (UserModel.findById as jest.Mock).mockResolvedValue(mockUser);
+            (RecipeModel.findById as jest.Mock).mockResolvedValue(mockRecipe);
+
+            await deleteSavedRecipe(req as Request, res as Response, next);
+
+            expect(mockUser.save).toHaveBeenCalled();
+            expect(res.json).toHaveBeenCalledWith({message: "Recipe has been deleted."});
+        });
+
+        it('should throw a ValidationError when no recipe matches the provided id', async () => {
+            (UserModel.findById as jest.Mock).mockResolvedValue(mockUser);
+            (RecipeModel.findById as jest.Mock).mockResolvedValue(null);
+
+            await deleteSavedRecipe(req as Request, res as Response, next);
+
+            expect(next).toHaveBeenCalledWith(new ValidationError('Recipe not found.'));
+        });
+
+        it('should throw a ValidationError when no user matches the provided id', async () => {
+            (UserModel.findById as jest.Mock).mockResolvedValue(null);
+            (RecipeModel.findById as jest.Mock).mockResolvedValue(mockRecipe);
+
+            await deleteSavedRecipe(req as Request, res as Response, next);
+
+            expect(next).toHaveBeenCalledWith(new ValidationError('User not found.'));
+        });
+    });
 });
+
